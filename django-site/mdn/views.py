90a08865abd76a2252f9e6cc4c71c6a1431f90c1
David Francoeur <dfrancoeur04@gmail.com>
2019-08-08 22:52:11 -0400

    Add a template with basic path parameters

--- django-site/mdn/views.py
+++ django-site/mdn/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the mdn index.")
+def index(request, name):
+    template = loader.get_template('index.html')
+    context = {
+        'name': name,
+    }
+    return HttpResponse(template.render(context, request))
--- django-site/mysite/settings.py
+++ django-site/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mdn.apps.MdnConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
